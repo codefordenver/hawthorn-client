24dc759dffeb2fc63485a4f5b90d6f004fdc0b1b
Trevor Smith <trevorcarringtonsmith@gmail.com>
2019-12-05 00:11:20 -0700

    Fix more typos

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export const About = (props) => {
     <div class="bg-white rounded shadow-sm ">
       <div class="m-3">
         <h1>What is Hawthorn?</h1>
-        <p>Hawthorn is a online community named for the Hawthorn tree, known in western herbalism for its heart healing properties, both physical and emotional.</p>
+        <p>Hawthorn is an online community named for the Hawthorn tree, known in western herbalism for its heart healing properties, both physical and emotional.</p>
         <p>Hawthorn is a place where anyone can gather to practice open and honest communication with other humans. This is a safe, cooperative place where you can find support from others who share similar life experiences to your own, while also developing empathy for those with life experiences adjacent to or completely separate from your own.  Hawthorn is a place to be seen, and to see others. Hawthorn is a place to heal.</p>
         <p>Hawthorn is based on the concept of <a href='https://en.wikipedia.org/wiki/Affinity_group' target='_blank'>affinity groups</a>, a group of individuals who come together around a shared experience, interest or common goal. Whether your group starts as friends or strangers, affinity groups often develop into a trusted circle of confidants, people you can go to share a life experience with or engage in facilitated conversation about the topics that are immediately affecting your life.</p>
         <p>In the initial version of Hawthorn we are piloting two affinity groups:</p>
@@ -19,7 +19,7 @@ export const About = (props) => {
         <h1>The Hawthorn Story</h1>
         <p>Hawthorn is inspired by an affinity group I’ve been incredibly lucky enough to be a part of in the physical world. Our group was formed during the 2018 Living Communities Design Course in Denver, Colorado through a shared experience: the members all benefit from the privilege of being male-bodied. Since we formed, we have built trust and a support system through shared emotional vulnerability.</p>
         <p>Through my experience working with our group, I have been able to identify, process, and rewrite many troubling stories I have long held about myself, and the people in my life and society as a whole.  I’ve learned how to strengthen my friendships and relationships, how to show up for myself, and how to ask for what I need.  I have been able to discover more of my true self and feel more content and comfortable in my life.  I have built new relationships based on trust and accountability and strengthened my existing relationships.  I have found a sense of community I had not previously known existed, and it has left me wanting more.</p>
-        <p>What has been most valuable in the work of our group is having a support system of people I can trust and can be openly vulnerable with. Seeing other people open themselves up has lowered my defenses and, in turn, has allowed me to open up.  This practice of sharing openly and honestly was foreign to me prior to being a part of our group, but now I have a support system that turns to each other in times of need.  It wasn’t until I began an intentional practice of sharing myself, asking for support, and returning the favor that I realized how my practice of stoicism was so petrifying and detrimental. Membership in a group like this has changed my life, and my mission has become facillitating connection and healing through Hawthorn.</p>
+        <p>What has been most valuable in the work of our group is having a support system of people I can trust and can be openly vulnerable with. Seeing other people open themselves up has lowered my defenses and, in turn, has allowed me to open up.  This practice of sharing openly and honestly was foreign to me prior to being a part of our group, but now I have a support system that turns to each other in times of need.  It wasn’t until I began an intentional practice of sharing myself, asking for support, and returning the favor that I realized how my practice of stoicism was so petrifying and detrimental. Membership in a group like this has changed my life, and my mission has become facilitating connection and healing through Hawthorn.</p>
         <p>As a parent, a father, and more specifically, the father of a gender expansive child, I see a need for a wider support system in these facets of my life.  Each day I am met with new opportunities and challenges that present themselves in puzzling and trying ways.  I need more support, and everyday I meet other parents, fathers, and parents of gender expansive children who also need more support.  With support and consent from my partner and child, this is my motivation to pilot Hawthorn with the Parents of Gender Expansive Children affinity group.</p>
         <p>Thank you for being here,</p>
         <p>trevor smith</p>
@@ -31,14 +31,14 @@ export const About = (props) => {
         <p>The Hawthorn software is open-source.  This means you can transparently see how we built it, and what we are doing with your data. Anyone can contribute to the project to make it better.</p>
         <h2>Help!</h2>
         <p>Interested in assisting to get this off the ground?  Want to facilitate and pilot a new Affinity Group?  Have a great idea for a new Feature?   Something not working the way you think it should?</p>
-        <p>Lets talk! We look forward to connecting.</p>
+        <p>Let's talk! We look forward to connecting.</p>
 
         <h1>Contributors</h1>
         Thank you to the following people and many others who have chosen to remain anonymous for putting your energy into this mission.
         <ul>
           <li><a href='https://github.com/cedewey' target='_blank'>Clayton Dewey</a> - Copy Editing</li>
           <li><a href='http://katemills.co/' target='_blank'>Kate Mills</a> - Product Design and Copy Editing</li>
-          <li><a href='https://github.com/moonquay' target='_blank'>moonquay</a> - Software Development, Product Design</li>
+          <li><a href='https://github.com/moonquay' target='_blank'>moonquay</a> - Software Development and Product Design</li>
           <li>trevor smith - Project Management and Software Development</li>
         </ul>
       </div>

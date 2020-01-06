6f8014b6ec2b7d0dc30d0c6cb7b52f36aea1b75f
trevor smith <trevorcarringtonsmith@gmail.com>
2020-01-06 15:59:45 -0700

    Update hero image

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,7 @@ import React from 'react'
 export const Header = (props) => {
   return (
     <div>
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container-fluid">
-          <h1 className="display-4">healing folks healing folks</h1>
-          <p className="lead text-wrap">Hawthorn is a safe environment, with focus on nurturing supportive communities,</p>
-          <p className="text-wrap">a platform with commitment to social justice, premised on mutual support and owned by you, the community</p>
-        </div>
-      </div>
+      <div className="jumbotron jumbotron-fluid" />
     </div>
   )
 }
--- src/components/Index.js
+++ src/components/Index.js
@@ -7,6 +7,8 @@ export const Index = withRouter((props) => {
   return (
     <div>
       <Header />
+      <p className="lead text-center">Hawthorn is a safe environment, with focus on nurturing supportive communities,</p>
+      <p className="text-center">a platform with commitment to social justice, premised on mutual support and owned by you, the community</p>
       <Groups />
     </div>
   )

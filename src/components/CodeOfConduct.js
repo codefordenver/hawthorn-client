bc5bd72cdf21c43d2d1b991eb0a1118f76402489
trevor smith <trevorcarringtonsmith@gmail.com>
2020-01-08 22:43:42 -0700

    Link Allied Media and Detroit Digital Coalition principles

--- src/components/CodeOfConduct.js
+++ src/components/CodeOfConduct.js
@@ -6,6 +6,7 @@ export const CodeOfConduct = (props) => {
       <div className="m-3">
         <h1>Hawthorn Code of Conduct</h1>
         <p>We are currently in conversation with community members and conducting research to draft our initial Code of Conduct.  Thank you for your patience.</p>
+        <p>Hawthorn observes and is committed to upholding <a href="https://www.alliedmedia.org/ddjc/principles" target='_blank'>the principles of The Detroit Digital Coalition</a> and <a href="https://www.alliedmedia.org/about/network-principles" target='_blank'>the Allied Media Network Principles</a>.</p>
         <p>For now and always, please be respectful to yourselves and one another.</p>
       </div>
     </div>

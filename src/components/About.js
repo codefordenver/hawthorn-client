80b6513b8e62ec83831662d4e7eab45f459f7fec
Zack <zack@Z16inch.local>
2020-02-07 01:04:59 -0700

    Protect target='_blank' links from malicious javascript

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,14 @@ export const About = (props) => {
         <h1>What is Hawthorn?</h1>
         <p>Hawthorn is an online community named for the Hawthorn tree, known in western herbalism for its heart healing properties, both physical and emotional.</p>
         <p>Hawthorn is a place where anyone can gather to practice open and honest communication with other humans from their communities. This is a safe, cooperative environment premised on mutual support.   Hawthorn is a place where you can find support from others who share similar life experiences to your own, while also developing empathy for those with life experiences adjacent to or completely separate from your own. Hawthorn is a place to be seen, and to see others. Hawthorn is a place to heal and grow.</p>
-        <p>Hawthorn is a <a href='https://www.onthecommons.org/work/what-commoning-anyway' target='_blank'>commons</a>, a shared community resource like a library or a park.  Hawthorn is an environment for people to gather, own and direct.  Hawthorn will be made together.</p>
-        <p>Hawthorn is based on the concept of <a href='https://en.wikipedia.org/wiki/Affinity_group' target='_blank'>affinity groups</a>, a group of individuals who come together around a shared experience, interest or common goal. Whether your group starts as friends or strangers, affinity groups often develop into a trusted circle of confidants, people you can go to share a life experience with or engage in facilitated conversation about the topics that are immediately affecting your life.</p>
+        <p>Hawthorn is a <a href='https://www.onthecommons.org/work/what-commoning-anyway' target='_blank' rel="noopener noreferrer">commons</a>, a shared community resource like a library or a park.  Hawthorn is an environment for people to gather, own and direct.  Hawthorn will be made together.</p>
+        <p>Hawthorn is based on the concept of <a href='https://en.wikipedia.org/wiki/Affinity_group' target='_blank' rel="noopener noreferrer">affinity groups</a>, a group of individuals who come together around a shared experience, interest or common goal. Whether your group starts as friends or strangers, affinity groups often develop into a trusted circle of confidants, people you can go to share a life experience with or engage in facilitated conversation about the topics that are immediately affecting your life.</p>
         <p>With Hawthorn affinity groups, you are free to start or join conversations and share useful resources with people in your community.  Find an awesome community resource in your neighborhood?  Having difficulty navigating something?  Organizing a political action?  Ask for support and share it with the wider community.</p>
-        <p>Conversation held within Hawthorn groups are considered confidential and private to the group. Your identity is anonymous by default. You decide what you want to to share about yourself and when. In striving to foster a safe space, a moderation process will be in place to counter bad actors and harmful communication.  A profanity, racism, and bullying filter has been implemented in the initial version of Hawthorn as a mitigation to harmful behavior.  Messages that have been screened will be reviewed by a moderator and posted if it is aligned with <a href="https://www.alliedmedia.org/ddjc/principles" target='_blank'>the principles of The Detroit Digital Coalition</a>, <a href="https://www.alliedmedia.org/about/network-principles" target='_blank'>the Allied Media Network Principles</a>, and our forthcoming <a href='/code-of-conduct'>Code of Conduct</a>.  The community members will review this process when a cooperative organization has been established governing Hawthorn.</p>
+        <p>Conversation held within Hawthorn groups are considered confidential and private to the group. Your identity is anonymous by default. You decide what you want to to share about yourself and when. In striving to foster a safe space, a moderation process will be in place to counter bad actors and harmful communication.  A profanity, racism, and bullying filter has been implemented in the initial version of Hawthorn as a mitigation to harmful behavior.  Messages that have been screened will be reviewed by a moderator and posted if it is aligned with <a href="https://www.alliedmedia.org/ddjc/principles" target='_blank' rel="noopener noreferrer">the principles of The Detroit Digital Coalition</a>, <a href="https://www.alliedmedia.org/about/network-principles" target='_blank' rel="noopener noreferrer">the Allied Media Network Principles</a>, and our forthcoming <a href='/code-of-conduct'>Code of Conduct</a>.  The community members will review this process when a cooperative organization has been established governing Hawthorn.</p>
 
         <h1>Join the community</h1>
         <h2>Hawthorn is owned and developed by you</h2>
-        <p>Hawthorn is committed to <a href="https://www.alliedmedia.org/ddjc/principles" target='_blank'>the principles of The Detroit Digital Coalition</a>, <a href="https://www.alliedmedia.org/about/network-principles" target='_blank'>the Allied Media Network Principles</a>, namely:
+        <p>Hawthorn is committed to <a href="https://www.alliedmedia.org/ddjc/principles" target='_blank' rel="noopener noreferrer">the principles of The Detroit Digital Coalition</a>, <a href="https://www.alliedmedia.org/about/network-principles" target='_blank' rel="noopener noreferrer">the Allied Media Network Principles</a>, namely:
           <ul>
             <li>Access</li>
             <li>Participation</li>
@@ -25,23 +25,23 @@ export const About = (props) => {
         <p>We are looking for community leaders to enter into long-term partnership with, who will inform, grow, and share in ownership of Hawthorn.  Specific skills are not important.  However, commitment to people, community, and social justice is.  If this sounds cool, <a href="/contact-us">please contact us</a>.</p>
         <p>Hawthorn is a regenerative enterprise, a cooperative business in the digital space, a platform cooperative.  In short, this means that the users and creators of Hawthorn will own, direct, and govern the platform together.  If this sounds cool, <a href="/contact-us">please contact us</a>.</p>
         <p>This is a socially democratic alternative to the prominent technology business model where a small handful of people own the software platform, the content/data its users produce, and with the primary motivation of making a return on their investment.  Under the cooperative model, if everyone votes to keep their data private and encrypted it will remain private and encrypted; if everyone votes to dramatically change the platform, so it will be.  The motivations of Hawthorn are solely to benefit people and the communities they are a part of.  If this sounds cool, <a href="/contact-us">please contact us</a>.</p>
-        <p>The Hawthorn software is open-source.  This means you can transparently see how we built it, and what we are doing with your data (the profanity and bullying filter is a closed source system (up for proposal)). Anyone can contribute to the project to make it better.  Check out the codebase for the <a href='https://github.com/trex/hawthorn-client' target='_blank'>frontend here</a>, and the <a href='https://github.com/trex/hawthorn-server' target='_blank'>backend here</a>.  More help with development and design is very welcome.  If this sounds cool, <a href="/contact-us">please contact us</a>.</p>
+        <p>The Hawthorn software is open-source.  This means you can transparently see how we built it, and what we are doing with your data (the profanity and bullying filter is a closed source system (up for proposal)). Anyone can contribute to the project to make it better.  Check out the codebase for the <a href='https://github.com/trex/hawthorn-client' target='_blank' rel="noopener noreferrer">frontend here</a>, and the <a href='https://github.com/trex/hawthorn-server' target='_blank' rel="noopener noreferrer">backend here</a>.  More help with development and design is very welcome.  If this sounds cool, <a href="/contact-us">please contact us</a>.</p>
         <p>If some of this doesn't sound cool, <a href="/contact-us">please contact us</a>.  We want to talk about it, understand why, and make this better.</p>
         <h1>Contributors</h1>
         Thank you to the following people and many others who have chosen to remain anonymous for putting your energy into this mission.
         <ul>
           <li>Michelle Castor</li>
-          <li><a href='https://www.instagram.com/hot4bergamot/' target='_blank'>Jada B.</a></li>
-          <li><a href='http://adambrock.me/' target='_blank'>Adam Brock</a></li>
-          <li><a href='https://github.com/cedewey' target='_blank'>Clayton Dewey</a></li>
-          <li><a href='https://communitycarecollective.com/' target='_blank'>Community Care Collective</a></li>
-          <li><a href='https://codefordenver.org/#/' target='_blank'>Code for Denver</a></li>
-          <li><a href='https://denverdevs.org/' target='_blank'>Denver Devs</a></li>
-          <li><a href='https://github.com/moonquay' target='_blank'>Monk DeVoe</a></li>
-          <li><a href='http://www.honeycombhealing.org/' target='_blank'>Kris Drickey</a></li>
+          <li><a href='https://www.instagram.com/hot4bergamot/' target='_blank' rel="noopener noreferrer">Jada B.</a></li>
+          <li><a href='http://adambrock.me/' target='_blank' rel="noopener noreferrer">Adam Brock</a></li>
+          <li><a href='https://github.com/cedewey' target='_blank' rel="noopener noreferrer">Clayton Dewey</a></li>
+          <li><a href='https://communitycarecollective.com/' target='_blank' rel="noopener noreferrer">Community Care Collective</a></li>
+          <li><a href='https://codefordenver.org/#/' target='_blank' rel="noopener noreferrer">Code for Denver</a></li>
+          <li><a href='https://denverdevs.org/' target='_blank' rel="noopener noreferrer">Denver Devs</a></li>
+          <li><a href='https://github.com/moonquay' target='_blank' rel="noopener noreferrer">Monk DeVoe</a></li>
+          <li><a href='http://www.honeycombhealing.org/' target='_blank' rel="noopener noreferrer">Kris Drickey</a></li>
           <li>suzi grishpul</li>
-          <li><a href='https://www.youtube.com/watch?v=be0WvqBFK3I' target='_blank'>Travis Heath</a></li>
-          <li><a href='http://katemills.co/' target='_blank'>Kate Mills</a></li>
+          <li><a href='https://www.youtube.com/watch?v=be0WvqBFK3I' target='_blank' rel="noopener noreferrer">Travis Heath</a></li>
+          <li><a href='http://katemills.co/' target='_blank' rel="noopener noreferrer">Kate Mills</a></li>
           <li>trevor smith</li>
         </ul>
       </div>
